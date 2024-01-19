bf3ad577f4305ba87c66b051e5114bc0692ed59a
Thomega35 <thomega35@gmail.com>
2024-01-19 23:34:08 +0100

    visualisation with ej2-react-charts

---
+++ visualizer/src/app/data.tsx
@@ -0,0 +1,14 @@
+export let chartData : any[] = [
+    { x: new Date(2012, 4, 2), open: 525.52, high: 527.77, low: 505.62, close: 506.88 },
+    { x: new Date(2012, 4, 3), open: 505.5, high: 513.5, low: 504.66, close: 509.46 },
+    { x: new Date(2012, 4, 4), open: 509.19, high: 520.98, low: 506.12, close: 518.42 },
+    { x: new Date(2012, 4, 7), open: 520.5, high: 526.29, low: 515.2, close: 522.7 },
+    { x: new Date(2012, 4, 8), open: 515.26, high: 517.47, low: 509.5, close: 509.96 },
+    { x: new Date(2012, 4, 9), open: 510.31, high: 515.25, low: 507.67, close: 511.3 },
+    { x: new Date(2012, 4, 10), open: 514.26, high: 515.25, low: 502, close: 505.38 },
+    { x: new Date(2012, 4, 11), open: 507.37, high: 512.5, low: 504, close: 509.46 },
+    { x: new Date(2012, 4, 14), open: 502.6, high: 509.44, low: 501.23, close: 507.46 },
+    { x: new Date(2012, 4, 15), open: 509.4, high: 511.89, low: 507.45, close: 509.73 },
+    { x: new Date(2012, 4, 16), open: 509.25, high: 514.98, low: 508.93, close: 514.85 },
+    { x: new Date(2012, 4, 17), open: 515.08, high: 517.83, low: 509, close: 509.96 },
+];
--- visualizer/src/app/page.tsx
+++ visualizer/src/app/page.tsx
@@ -7,6 +7,7 @@ import { data } from "~/lib/data"
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b text-black bg-white">
+      <link href='https://cdn.syncfusion.com/ej2/22.1.34/material.css' rel='stylesheet'></link>
       <Graph data={data} />
     </main>
   );
